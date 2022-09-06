9f4486c857ddbf68f6078b732cd71d7276c6e20b
toufik11524 <toufikur.rahman@bjitgroup.com>
2022-09-06 16:46:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Webpack</h1>
+      <p>Create React App without create-react-app</p>
+    </div>
+  )
+}
+
+export default App
